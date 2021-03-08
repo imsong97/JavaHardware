45196e07a2f96f812f66e2f14aa16dc5b660f835
DyNamiC <imsong1997@gmail.com>
2021-03-08 13:04:24 +0900

    cpu usage

--- JavaHardware/src/JavaHardware.java
+++ JavaHardware/src/JavaHardware.java
@@ -1,3 +1,7 @@
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.lang.management.RuntimeMXBean;
+
 public class JavaHardware {
     public static void main(String args[]) {
         System.out.println();
@@ -14,5 +18,23 @@ public class JavaHardware {
         System.out.println(System.getProperty(versionOS));
         System.out.println(System.getProperty(architectureOS));
         System.out.println(System.getProperty("user.name"));
+
+        /* 윈도우 동작X
+        OperatingSystemMXBean osbean = ( OperatingSystemMXBean ) ManagementFactory.getOperatingSystemMXBean( );
+        RuntimeMXBean runbean = ManagementFactory.getRuntimeMXBean( );
+        long bfprocesstime = osbean.getProcessCpuTime();
+        long bfuptime = runbean.getUptime( );
+        long ncpus = osbean.getAvailableProcessors( );
+        for ( int i = 0 ; i < 1000000 ; ++i )
+        {
+            ncpus = osbean.getAvailableProcessors( );
+        }
+        long afprocesstime = osbean.getProcessCpuTime();
+        long afuptime = runbean.getUptime( );
+        float cal = ( afprocesstime - bfprocesstime ) / ( ( afuptime - bfuptime ) * 10000f );
+        float usage = Math.min( 99f , cal );
+        System.out.println( "Calculation: " + cal );
+        System.out.println( "CPU Usage: " + usage );
+        */
     }
 }
