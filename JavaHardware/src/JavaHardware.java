c185f5ac6f6a3b25ba0b985512d4c4d5a230317e
DyNamiC <imsong1997@gmail.com>
2021-03-08 13:14:04 +0900

    cpu, memory, thread usage

--- JavaHardware/src/JavaHardware.java
+++ JavaHardware/src/JavaHardware.java
@@ -1,6 +1,5 @@
-import java.lang.management.ManagementFactory;
-import java.lang.management.OperatingSystemMXBean;
-import java.lang.management.RuntimeMXBean;
+import java.io.File;
+import java.lang.management.*;
 
 public class JavaHardware {
     public static void main(String args[]) {
@@ -9,7 +8,6 @@ public class JavaHardware {
         /* 참고 사이트
         https://www.roseindia.net/java/beginners/OSInformation.shtml
         */
-
         String nameOS = "os.name";
         String versionOS = "os.version";
         String architectureOS = "os.arch";
@@ -18,23 +16,67 @@ public class JavaHardware {
         System.out.println(System.getProperty(versionOS));
         System.out.println(System.getProperty(architectureOS));
         System.out.println(System.getProperty("user.name"));
+        System.out.println();
 
-        /* 윈도우 동작X
-        OperatingSystemMXBean osbean = ( OperatingSystemMXBean ) ManagementFactory.getOperatingSystemMXBean( );
-        RuntimeMXBean runbean = ManagementFactory.getRuntimeMXBean( );
-        long bfprocesstime = osbean.getProcessCpuTime();
-        long bfuptime = runbean.getUptime( );
-        long ncpus = osbean.getAvailableProcessors( );
-        for ( int i = 0 ; i < 1000000 ; ++i )
+/*
+==============================================================================================================================================
+*/
+
+        /* CPU 사용량 - 윈도우 동작X */
+//        OperatingSystemMXBean osbean = ( OperatingSystemMXBean ) ManagementFactory.getOperatingSystemMXBean( );
+//        RuntimeMXBean runbean = ManagementFactory.getRuntimeMXBean( );
+//        long bfprocesstime = osbean.getProcessCpuTime();
+//        long bfuptime = runbean.getUptime( );
+//        long ncpus = osbean.getAvailableProcessors( );
+//        for ( int i = 0 ; i < 1000000 ; ++i )
+//        {
+//            ncpus = osbean.getAvailableProcessors( );
+//        }
+//        long afprocesstime = osbean.getProcessCpuTime();
+//        long afuptime = runbean.getUptime( );
+//        float cal = ( afprocesstime - bfprocesstime ) / ( ( afuptime - bfuptime ) * 10000f );
+//        float usage = Math.min( 99f , cal );
+//        System.out.println( "Calculation: " + cal );
+//        System.out.println( "CPU Usage: " + usage );
+
+
+        /* 메모리 사용량 */
+        MemoryMXBean membean = ManagementFactory.getMemoryMXBean( );
+        MemoryUsage heap = membean.getHeapMemoryUsage( );
+        System.out.println( "Heap Memory: " + heap.toString( ) );
+        MemoryUsage nonheap = membean.getNonHeapMemoryUsage( );
+        System.out.println( "NonHeap Memory: " + nonheap.toString( ) );
+
+        ClassLoadingMXBean classbean = ManagementFactory.getClassLoadingMXBean( );
+        System.out.println( "TotalLoadedClassCount: " + classbean.getTotalLoadedClassCount( ) );
+        System.out.println( "LoadedClassCount: " + classbean.getLoadedClassCount( ) );
+        System.out.println( "UnloadedClassCount: " + classbean.getUnloadedClassCount( ) );
+        System.out.println();
+
+
+        /* 디스크 사용량 */
+        File root = null;
+        try
         {
-            ncpus = osbean.getAvailableProcessors( );
+            root = new File( "C:/" );
+            System.out.println( "Total  Space: " + root.getTotalSpace( ) );
+            System.out.println( "Usable Space: " + root.getUsableSpace( ) );
+            System.out.println();
         }
-        long afprocesstime = osbean.getProcessCpuTime();
-        long afuptime = runbean.getUptime( );
-        float cal = ( afprocesstime - bfprocesstime ) / ( ( afuptime - bfuptime ) * 10000f );
-        float usage = Math.min( 99f , cal );
-        System.out.println( "Calculation: " + cal );
-        System.out.println( "CPU Usage: " + usage );
-        */
+        catch ( Exception e )
+        {
+            e.printStackTrace( );
+        }
+
+
+        /* 스레드 사용 */
+        ThreadMXBean tbean = ManagementFactory.getThreadMXBean( );
+        long[] ids = tbean.getAllThreadIds( );
+        System.out.println( "Thread Count: " + tbean.getThreadCount( ) );
+        for ( long id : ids )
+        {
+            System.out.println( "Thread CPU Time(" + id + ")" + tbean.getThreadCpuTime( id ) );
+            System.out.println( "Thread User Time(" + id + ")" + tbean.getThreadCpuTime( id ) );
+        }
     }
 }
